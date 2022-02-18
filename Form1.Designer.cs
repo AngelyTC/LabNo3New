321113812bd3d74a0a66657968f0062b4531bcb6
AngelyTC <angelyth312@mesoamericana.edu.gt>
2022-02-17 20:07:13 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace LabNo3New
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbBuscar = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbBuscar
+            // 
+            this.cmbBuscar.FormattingEnabled = true;
+            this.cmbBuscar.Location = new System.Drawing.Point(25, 34);
+            this.cmbBuscar.Name = "cmbBuscar";
+            this.cmbBuscar.Size = new System.Drawing.Size(524, 28);
+            this.cmbBuscar.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(25, 82);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(725, 340);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(562, 32);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(55, 29);
+            this.btnIr.TabIndex = 2;
+            this.btnIr.Text = "Ir";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 607);
+            this.Controls.Add(this.btnIr);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.cmbBuscar);
+            this.Name = "Form1";
+            this.Text = "Navegador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBuscar;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnIr;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LabNo3New
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+
+            String uri = cmbBuscar.Text;
+
+            //Angely Esmeralda Thomas Cortéz -  202108047 - Ingeniería en Sistemas 
+            //Tercer Semestre Universidad Mesoamericana, Programación
+
+            //condicion para url
+            if (uri.Contains("."))
+            {
+                if (uri.Contains("https://"))
+                {
+                    //Buscar lo seleccionado en el combobox
+                    webBrowser1.Navigate(new Uri(cmbBuscar.Text));
+                    Guardar(@"C:\Users\USUARIO\Documents\ArchivoLab3.txt", uri);
+                    cmbBuscar.Items.Add(uri);
+                }
+                else
+                {
+                    webBrowser1.Navigate(new Uri("https://" + cmbBuscar.Text));
+                    cmbBuscar.Text = "https://" + cmbBuscar.Text;
+                    Guardar(@"C:\Users\USUARIO\Documents\ArchivoLab3.txt", uri);
+                    cmbBuscar.Items.Add(uri);
+                }
+            }
+            else
+            {
+                uri = "http://www.google.com/search?q=" + uri;
+                webBrowser1.Navigate(new Uri(uri));
+            }
+           
+
+        }
+
+        private void Guardar(string nombreArchivo, string texto)
+        {
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(texto);
+
+            writer.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string nombreArchivo = @"C:\Users\USUARIO\Documents\ArchivoLab3.txt";
+
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                string texto = reader.ReadLine();
+               cmbBuscar.Items.Add(texto);
+            }
+            reader.Close();
+        }
+    }
+}
